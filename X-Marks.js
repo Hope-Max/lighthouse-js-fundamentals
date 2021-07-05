8f2d2ff3c4e052f645dd1c706dd4c7debf99cb49
Max Li <hope_max@outlook.com>
2021-07-04 20:21:46 -0400

    Add X-Marks.js

---
+++ X-Marks.js
@@ -0,0 +1,23 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east', 'north'];
+
+const finalPosition = function(moves) {
+  let x = 0;
+  let y = 0;
+  let position = [];
+
+  for (const move of moves) {
+    if (move === 'north') {
+      y++;
+    } else if (move === 'south') {
+      y--;
+    } else if (move === 'east') {
+      x++;
+    } else if (move === 'west') {
+      x--;
+    }
+  }
+  position[0] = x;
+  position[1] = y;
+  return position;
+}
+console.log(finalPosition(moves));
